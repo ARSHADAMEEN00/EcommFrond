f8ef6616ce7b95ebb2a7999a2f2108c37b9ff713
Hisham Paloli <hishampaloli786@gmail.com>
2022-09-14 09:38:50 +0530

    productReducer

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,17 +1,30 @@
-import React from 'react'
-import products from '../products'
-import Product from '../components/Product'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Product from "../components/Product";
 
 const HomeScreen = () => {
-  return (
-    <div style={{margin: '20px'}} className='flex-r'>
-        {products.map(product => {
-            return <div>
-                <Product product={product} />
-            </div>
-        })}
-    </div>
-  )
-}
+  const [products, setProducts] = useState([]);
 
-export default HomeScreen
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get("/api/products");
+      console.log(data);
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+
+  return (
+    <div style={{ margin: "20px" }} className="flex-r">
+      {products.map((product) => {
+        return (
+          <div>
+            <Product product={product} />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default HomeScreen;
--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,27 +1,54 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import Rating from '../components/Rating'
-import products from '../products';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import Rating from "../components/Rating";
 
-const ProductScreen = ({match}) => {
-    const product = products.find(p => p._id === match.params.id);
+const ProductScreen = ({ match }) => {
+  const [product, setProduct] = useState("");
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`);
+      console.log(data);
+      setProduct(data);
+    };
+
+    fetchProducts();
+  }, []);
   return (
     <div>
-        <Link to='/' >Go Back</Link>
-        <div style={{display: 'flex'}}>
-            <div>
-                <img  style={{ width: "400px", marginLeft: '50px' }}  src={product.image} alt="" />
-            </div>
-
-            <div>
-                    <h2>{product.name}</h2> 
-                    <Rating val={product.rating} text={`${product.numReviews} reviews`} />
-                    <h2>{product.price}</h2>
-                    <p>{product.description}</p>
-            </div>
+      <Link to="/">Go Back</Link>
+      <div style={{ display: "flex" }}>
+        <div>
+          <img
+            style={{ width: "400px", marginLeft: "50px" }}
+            src={product.image}
+            alt=""
+          />
         </div>
-    </div>
-  )
-}
 
-export default ProductScreen
+        <div>
+          <h2>{product.name}</h2>
+          <Rating val={product.rating} text={`${product.numReviews} reviews`} />
+          <h2>Price:${product.price}</h2>
+          <p>{product.description}</p>
+        </div>
+
+        <div>
+          <p>
+            Status: {product.countInStock > 0 ? "In Stock" : "Out of Stock"}{" "}
+          </p>
+          <button
+            className="btn"
+            type="button"
+            disabled={product.countInStock === 0}
+          >
+            Add to cart
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductScreen;
